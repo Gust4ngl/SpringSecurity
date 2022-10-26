86fc37df10e01d93a5c78e01a302b27d28205bb8
Gust4ngl <gustavosilvaskt@outlook.com>
2022-10-26 09:45:59 -0300

    learning more about spring security

--- src/main/java/com/gust4/jwt/config/ApplicationSecurityConfig.java
+++ src/main/java/com/gust4/jwt/config/ApplicationSecurityConfig.java
@@ -1,20 +1,46 @@
 package com.gust4.jwt.config;
 
+import org.springframework.beans.factory.annotation.*;
 import org.springframework.context.annotation.*;
 import org.springframework.security.config.annotation.web.builders.*;
 import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.security.crypto.password.*;
+import org.springframework.security.provisioning.*;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/hello/user").permitAll()
+                .antMatchers("/" ,"index" , "/hello/user").permitAll()
                 .antMatchers("/hello/adm").authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+       UserDetails gustaUser = User.builder()
+               .username("Gusta")
+               .password(passwordEncoder.encode("password"))
+               .roles("STUDENT") //ROLE_STUDENT
+               .build();
+
+       return new InMemoryUserDetailsManager(
+               gustaUser
+       );
+
     }
 }
---
+++ src/main/java/com/gust4/jwt/config/PasswordConfig.java
@@ -0,0 +1,15 @@
+package com.gust4.jwt.config;
+
+import org.springframework.context.annotation.*;
+import org.springframework.security.crypto.bcrypt.*;
+import org.springframework.security.crypto.password.*;
+
+@Configuration
+public class PasswordConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+
+}
