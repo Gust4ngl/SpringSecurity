4f726494d3062a0a4d81ae150ba17bceca98e9e9
Gust4ngl <gustavosilvaskt@outlook.com>
2022-11-22 19:25:12 -0300

    JWT

--- src/main/java/com/gust4/springbootsecurity/config/SecurityConfig.java
+++ src/main/java/com/gust4/springbootsecurity/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.gust4.springbootsecurity.config;
 
+import com.gust4.springbootsecurity.jwt.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.context.annotation.*;
 import org.springframework.security.config.annotation.authentication.builders.*;
 import org.springframework.security.config.annotation.web.builders.*;
 import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.config.http.*;
 import org.springframework.security.core.userdetails.*;
 import org.springframework.security.crypto.bcrypt.*;
 import org.springframework.security.crypto.password.*;
@@ -31,30 +33,34 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilter(new JwtUsernameAndPasswordAuthAndFilter(authenticationManager()))
                 .authorizeRequests()
                 .antMatchers("/").permitAll()
                 .antMatchers("/hello/adm").hasAuthority("ADMIN")
                 .antMatchers("/hello/trainee").hasAuthority("TRAINEE")
                 .antMatchers("/hello/user").hasAuthority("COMMON_USER")
                 .anyRequest()
-                .authenticated()
-                .and()
-                .formLogin()
-                    .loginPage("/login").permitAll()
-                    .defaultSuccessUrl("/", true)
-                    .passwordParameter("password")
-                    .usernameParameter("username")
-                .and()
-                .rememberMe()
-                    .rememberMeParameter("remember-me")
-                .and()
-                .logout()
-                .logoutUrl("/logout")
-                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))//if crsf is disabled. If crsf is enabled, delete this line
-                    .clearAuthentication(true)
-                    .invalidateHttpSession(true)
-                    .deleteCookies("JSESSIONID", "remember-me")
-                    .logoutSuccessUrl("/login");
+                .authenticated();
+
+//                .formLogin()
+//                    .loginPage("/login").permitAll()
+//                    .defaultSuccessUrl("/", true)
+//                    .passwordParameter("password")
+//                    .usernameParameter("username")
+//                .and()
+//                .rememberMe()
+//                    .rememberMeParameter("remember-me")
+//                .and()
+//                .logout()
+//                .logoutUrl("/logout")
+//                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))//if crsf is disabled. If crsf is enabled, delete this line
+//                    .clearAuthentication(true)
+//                    .invalidateHttpSession(true)
+//                    .deleteCookies("JSESSIONID", "remember-me")
+//                    .logoutSuccessUrl("/login");
     }
 
 }
--- src/main/java/com/gust4/springbootsecurity/jwt/JwtUsernameAndPasswordAuthAndFilter.java
+++ src/main/java/com/gust4/springbootsecurity/jwt/JwtUsernameAndPasswordAuthAndFilter.java
@@ -1,12 +1,17 @@
 package com.gust4.springbootsecurity.jwt;
 
 import com.fasterxml.jackson.databind.*;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.*;
 import org.springframework.security.authentication.*;
 import org.springframework.security.core.*;
 import org.springframework.security.web.authentication.*;
 
+import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.*;
+import java.time.*;
+import java.util.*;
 
 public class JwtUsernameAndPasswordAuthAndFilter extends UsernamePasswordAuthenticationFilter {
     private final AuthenticationManager authenticationManager;
@@ -21,9 +26,28 @@ public class JwtUsernameAndPasswordAuthAndFilter extends UsernamePasswordAuthent
         try {
             UsernameAndPasswordAuthRequest authenticationRequest = new ObjectMapper().
                     readValue(request.getInputStream(), UsernameAndPasswordAuthRequest.class);
+            Authentication authentication = new UsernamePasswordAuthenticationToken(
+                    authenticationRequest.getUsername(),
+                    authenticationRequest.getPassword()
+            );
+            return authenticationManager.authenticate(authentication);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        return super.attemptAuthentication(request, response);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
+        String token = Jwts.builder()
+                .setSubject(authResult.getName())
+                .claim("authorities", authResult.getAuthorities())
+                .setIssuedAt(new Date())
+                .setExpiration(java.sql.Date.valueOf(LocalDate.now().plusWeeks(2)))
+                .signWith(Keys.secretKeyFor(SignatureAlgorithm.HS256))
+                .compact();
+        response.addHeader("Authorization", "Bearer " + token);
     }
 }
